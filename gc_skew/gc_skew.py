cd93711ac8f818f8c050d0967cd0fb181f5049cf
Manuel Gehl <manuelgehl@gmail.com>
2024-03-12 08:04:45 +0100

    Added docstrings, plot title and simplyfied.

--- gc_skew/gc_skew.py
+++ gc_skew/gc_skew.py
@@ -2,36 +2,64 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 
 def calculate_gc_skew(sequence: str) -> list:
+    """
+    Calculate GC skew scores for each position in the sequence.
+
+    Parameters:
+    - sequence (str): DNA sequence.
+
+    Returns:
+    - list: List of GC skew scores.
+    """
     # Initialize skew skew_array with 0
     skew_array = [0]
+    
     # Iterate over sequence and add scores if Cytosine or Guanine appear
     for pos, nucleotide in enumerate(sequence):
         if nucleotide == "C":
             current_sum = skew_array[pos] - 1
-            skew_array.append(current_sum)
         elif nucleotide == "G":
             current_sum = skew_array[pos] + 1
-            skew_array.append(current_sum)
         else:
             current_sum = skew_array[pos] + 0
-            skew_array.append(current_sum)
 
+        # Append current valute to array
+        skew_array.append(current_sum)
+        
     return skew_array
 
 def plot_skew(skew_array: list) -> None:
+    """
+    Plot GC skew scores as a function of positions in the genome.
+
+    Parameters:
+    - skew_array (list): List of GC skew scores.
+    """
     # Plot GC skew as function of positions in genome
-    sns.set_style()
+    sns.set_style("ticks")
     plt.xlabel("Position")
     plt.ylabel("GC Skew Score")
+    plt.title("GC Skew Analysis")
     plt.plot(skew_array)
     plt.show()
 
-def min_max_skew(skew_array: list) -> list, list:
+def min_max_skew(skew_array: list) -> list:
+    """
+    Calculate minimum and maximum values of GC skew.
+
+    Parameters:
+    - skew_array (list): List of GC skew scores.
+
+    Returns:
+    - list: Positions where the skew is minimum.
+    - list: Positions where the skew is maximum.
+    """
     # Calculate minimum and maximum values of GC skew
     minimum_skew = min(skew_array)
     maximum_skew = max(skew_array)
+    
     # Determines all positions where the skew is minimum and maximum
-    minimum_positions = [index for index in range(len(skew_array)) if skew_array[index] == minimum_skew]
-    maximum_positions = [index for index in range(len(skew_array)) if skew_array[index] == maximum_skew]
+    minimum_positions = [index for index, skew in enumerate(skew_array) if skew == minimum_skew]
+    maximum_positions = [index for index, skew in enumerate(skew_array) if skew == maximum_skew]
     
     return minimum_positions, maximum_positions
