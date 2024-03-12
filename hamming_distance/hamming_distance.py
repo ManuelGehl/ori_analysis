12e917def953e6de6061132d43f0b3ac6e81b28b
Manuel Gehl <manuelgehl@gmail.com>
2024-03-12 07:58:32 +0100

    Changed verbose to bool

--- hamming_distance/hamming_distance.py
+++ hamming_distance/hamming_distance.py
@@ -1,6 +1,10 @@
-def hamming_distance(sequence1: str, sequence2: str, verbose:int = 0) -> int:
+def hamming_distance(sequence1: str, sequence2: str, verbose:bool = False) -> int:
     """
     Measures hamming distance between 2 sequences.
+    
+    Example:
+    >>> hamming_distance("AGC", "ATC", verbose=True)
+    Hamming distance: 1
     """
     # Check if sequences are of equal length
     if len(sequence1) != len(sequence2):
@@ -10,13 +14,8 @@ def hamming_distance(sequence1: str, sequence2: str, verbose:int = 0) -> int:
     for i in range(len(sequence1)):
         if sequence1[i] != sequence2[i]:
             hamming_dist += 1
-    if verbose == 1:
+    # Print resulting hamming distance        
+    if verbose:
         print(f"Hamming distance: {hamming_dist}")
     
     return hamming_dist
-
-
-seq1 = "TGTGAGAGTGGT"
-seq2 = "ACCTCGAATAGT"
-
-hamming_distance(seq1, seq2)
