9a08a2810e9f9660127567a37cf56541bb6531f1
Manuel Gehl <manuelgehl@gmail.com>
2024-03-12 08:45:44 +0100

    Added k_mer generation.

--- generate_k_mers/generate_k_mers.py
+++ generate_k_mers/generate_k_mers.py
@@ -1,3 +1,20 @@
-def generate_k_mers(sequence: str, k: int, start: int = 0, stop: int = 100) -> list:
+def generate_k_mers(sequence: str, k_mer_length: int, range: tuple = (0, 100)) -> list:
+    
+    # Define part of sequence to generate k-mers from
+    start, stop = range
+    sequence_part = sequence[start:stop + 1]
+    
+    # Define scanning range
+    scanning_range = len(sequence_part) - k_mer_length + 1
+    
     k_mer_list = []
-    for pos in range(len(sequence))
+    # Slide a window over the sequence part
+    for pos in range(scanning_range):
+        # Add current k-mer to list
+        k_mer_list.append(sequence[pos:pos + k_mer_length])
+    
+    # Remove duplicates
+    k_mer_list = list(set(k_mer_list))
+    
+    return k_mer_list
+        
